fa1802822dd9140c6816b3181167090f0fa663ae
Ibrahimsuf <110345830+Ibrahimsuf@users.noreply.github.com>
2025-09-12 04:59:33 -0500

    allow elo update on win or loss

--- app/models/food.rb
+++ app/models/food.rb
@@ -1,2 +1,13 @@
 class Food < ApplicationRecord
+  def win(food2)
+    update(elo: elo+K*(1-win_probability(food2)))
+  end
+  def loss(food2)
+     update(elo: elo-K*win_probability(food2))
+  end
+  private
+  K = 32
+  def win_probability(food2)
+    1.0/(1.0 + 10.0 ** ((elo - food2.elo)/400.0))
+  end
 end
--- test/models/food_test.rb
+++ test/models/food_test.rb
@@ -1,7 +1,16 @@
 require "test_helper"
 
 class FoodTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "correct updates the elo on a win" do
+    food1 = Food.new(name: "Sample", image_filename: "Sample", elo: 1613)
+    food2 = Food.new(name: "Sample2", image_filename: "Sample2", elo: 1609)
+    food1.win(food2)
+    assert (food1.elo - 1628).abs < 2
+  end
+  test "correctly updates the elo on a loss" do
+    food1 = Food.new(name: "Sample", image_filename: "Sample", elo: 1613)
+    food2 = Food.new(name: "Sample2", image_filename: "Sample2", elo: 1609)
+    food1.loss(food2)
+    assert (food1.elo - 1596.68).abs < 2
+  end
 end
