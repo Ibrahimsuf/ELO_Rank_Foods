c270429d3d9ae9367f83952a64a56b5d0e3b0e49
Ibrahimsuf <110345830+Ibrahimsuf@users.noreply.github.com>
2025-09-12 06:33:12 -0500

    pick random starting food

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -3,7 +3,7 @@ class FoodsController < ApplicationController
 
   # GET /foods or /foods.json
   def index
-    @foods = Food.all
+    @foods = Food.order(elo: :desc)
   end
 
   # GET /foods/1 or /foods/1.json
@@ -45,6 +45,13 @@ class FoodsController < ApplicationController
       @food.lose(food2)
       food2.win(@food)
     end
+    next_foods = Food.all.sample(2)
+    redirect_to(edit_food_path(id: next_foods[0].id))
+  end
+
+  def random_edit
+    food = Food.order("RANDOM()").first
+    redirect_to edit_food_path(food)
   end
 
   # DELETE /foods/1 or /foods/1.json
